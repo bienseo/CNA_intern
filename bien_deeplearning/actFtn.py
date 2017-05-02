ad6dee8a4f843da375eef07390b79443461e9401
bienseo <bienseo04@gmail.com>
2017-05-02 23:07:21 +0900

    Activation Function
    Step function, sigmoid, ReLu

---
+++ bien_deeplearning/actFtn.py
@@ -0,0 +1,41 @@
+'''
+CNA Intern
+Study name: Activation Function
+Created by Eunseo Cho on 02/05/2017.
+'''
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def step_function(x):
+    y = x > 0
+    #  print(y)
+    #  print(y.astype(np.int))
+    return y.astype(np.int)
+
+def sigmoid(x):
+	h = 1 / (1 + np.exp(-x))
+	return h
+
+def relu(x):
+	h = np.maximum(0, x)
+	return h
+
+def main():
+    x1 = np.array([1, 2, 3])
+    y1 = step_function(x1)
+    
+    x = np.arange(-5,5,0.5)
+    y = step_function(x)
+    f = np.arange(-5,5,0.5)
+    g = sigmoid(f)
+    fi = f
+    fo = relu(fi)
+
+
+    plt.plot(x,y,f,g,fi,fo)
+    plt.show()
+
+
+if __name__ == '__main__':
+	main()
