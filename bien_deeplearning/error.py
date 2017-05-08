1df9fc5b749420bb593574e36329c1528738c7c4
bienseo <bienseo04@gmail.com>
2017-05-08 14:35:58 +0900

    Error for batch
    Error for batch

--- bien_deeplearning/error.py
+++ bien_deeplearning/error.py
@@ -11,8 +11,32 @@ def mean_squared_error(y, t):
 
 	return e
 
+'''
 def cross_entropy_error(y, t):
 	delta = 1e-7
 	e = -np.sum(t * np.log(y + delta))
 
 	return e
+'''
+'''
+def cross_entropy_error(y, t):
+	if y.ndim == 1:
+		t = t.reshape(1, t.size)
+		y = y.reshape(1, y.size)
+
+	batch_size = y.shape[0]	
+	e = -np.sum(t * np.log(y)) / batch_size
+
+	return e	
+'''
+
+def cross_entropy_error(y, t):
+	if y.ndim == 1:
+		t = t.reshape(1, t.size)
+		y = y.reshape(1, y.size)
+
+	batch_size = y.shape[0]	
+	e = -np.sum(np.log(y[np.arange(batch_size), t])) / batch_size
+
+	return e	
+
