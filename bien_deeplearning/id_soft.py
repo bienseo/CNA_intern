dc83c52971a5fcdcf906a52609ba2995c1bf9841
bienseo <bienseo04@gmail.com>
2017-05-15 14:11:51 +0900

    softmax_ftn 수정
    softmax_ftn 수정

--- bien_deeplearning/id_soft.py
+++ bien_deeplearning/id_soft.py
@@ -11,7 +11,7 @@ def identity_ftn(a):
 	y = a
 
 	return y
-
+'''
 def softmax_ftn(a):
 	c = np.max(a)
 	exp_a = np.exp(a - c) # prevent overflow
@@ -19,3 +19,13 @@ def softmax_ftn(a):
 	y = exp_a / sum_exp_a
 
 	return y
+'''
+def softmax_ftn(x):
+    if x.ndim == 2:
+        x = x.T
+        x = x - np.max(x, axis=0)
+        y = np.exp(x) / np.sum(np.exp(x), axis=0)
+        return y.T 
+
+    x = x - np.max(x) # 오버플로 대책
+    return np.exp(x) / np.sum(np.exp(x))	
